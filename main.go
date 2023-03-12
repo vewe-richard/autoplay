1806f1a5e85eadafb329f51ec1e02efa0e6adf69
richard <jiang.j.qian@qq.com>
2023-03-12 14:44:36 +0800

    a fix

--- main.go
+++ main.go
@@ -147,7 +147,7 @@ func main() {
 }
 
 func timestr() string {
-	day := []string{"", "一", "二", "三", "四", "五", "六", "日"}
+	day := []string{"日", "一", "二", "三", "四", "五", "六"}
 	now := time.Now()
 	return fmt.Sprintf(" %s    %02d:%02d  ", day[now.Weekday()], now.Hour(), now.Minute())
 }
